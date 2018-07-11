6661e8e48f0e979f57dae019662b5f0b31537d7d
aidanlee07 <godogepower07@gmail.com>
2018-07-11 16:39:00 -0700

    changed

---
+++ drawing.js
@@ -0,0 +1,9 @@
+var canvas;
+var ctx;
+window.onload = init; // When the window loads, run the init function
+function init(){
+    canvas= document.getElementById("myCanvas")
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = 'blue'; // Setting the colour of filling
+    ctx.fillRect(0,0,100,100); // Drawing a rectangle
+}
