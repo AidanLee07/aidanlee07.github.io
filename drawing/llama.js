ef313173369d75b0d3976a092153377fbb242223
aidanlee07 <godogepower07@gmail.com>
2019-01-23 18:36:57 -0800

    added animation for the llama and added more for the llama

--- drawing/llama.js
+++ drawing/llama.js
@@ -1,7 +1,10 @@
-function drawLlama() {
+function drawLlama(x, y) {
+    ctx.save();
+    ctx.translate(x * unit, y * unit);
     drawHead();
     drawBody();
     drawLeg();
+    ctx.restore();
 }
 
 function drawHead() {
@@ -143,6 +146,24 @@ function drawBody() {
     ctx.fillRect(43 * unit, 41 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(45 * unit, 41 * unit, 3 * unit, 1 * unit);
     ctx.fillRect(44 * unit, 42 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(17 * unit, 47 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(20 * unit, 46 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(22 * unit, 46 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(24 * unit, 47 * unit, 2 * unit, 1 * unit);
+    ctx.fillRect(17 * unit, 48 * unit, 9 * unit, 1 * unit);
+    ctx.fillRect(15 * unit, 48 * unit, 11 * unit, 7 * unit);
+    ctx.fillRect(16 * unit, 55 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(18 * unit, 55 * unit, 2 * unit, 2 * unit);
+    ctx.fillRect(20 * unit, 55 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(21 * unit, 55 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(22 * unit, 55 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(24 * unit, 55 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(39 * unit, 46 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(40 * unit, 47 * unit, 1 * unit, 9 * unit);
+    ctx.fillRect(41 * unit, 43 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(48 * unit, 42 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(45 * unit, 43 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(49 * unit, 43 * unit, 1 * unit, 2 * unit);
     ctx.fillStyle = "rgb(96,61,142)";//dark purple
     ctx.fillRect(5 * unit, 36 * unit, 10 * unit, 1 * unit);
     ctx.fillRect(5 * unit, 37 * unit, 10 * unit, 1 * unit);
@@ -150,6 +171,24 @@ function drawBody() {
     ctx.fillRect(44 * unit, 41 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(42 * unit, 42 * unit, 2 * unit, 1 * unit);
     ctx.fillRect(45 * unit, 42 * unit, 3 * unit, 1 * unit);
+    ctx.fillRect(4 * unit, 47 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(14 * unit, 47 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(2 * unit, 48 * unit, 3 * unit, 1 * unit);
+    ctx.fillRect(6 * unit, 48 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(11 * unit, 48 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(2 * unit, 49 * unit, 5 * unit, 1 * unit);
+    ctx.fillRect(8 * unit, 49 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(2 * unit, 50 * unit, 13 * unit, 3 * unit);
+    ctx.fillRect(2 * unit, 53 * unit, 2 * unit, 1 * unit);
+    ctx.fillRect(5 * unit, 53 * unit, 10 * unit, 1 * unit);
+    ctx.fillRect(2 * unit, 54 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(6 * unit, 54 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(8 * unit, 54 * unit, 7 * unit, 1 * unit);
+    ctx.fillRect(8 * unit, 55 * unit, 2 * unit, 1 * unit);
+    ctx.fillRect(11 * unit, 55 * unit, 2 * unit, 2 * unit);
+    ctx.fillRect(13 * unit, 55 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(14 * unit, 55 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(40 * unit, 43 * unit, 1 * unit, 1 * unit);
     ctx.fillStyle = "rgb(44,119,165)";// dark sky blue
     ctx.fillRect(4 * unit, 35 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(6 * unit, 35 * unit, 1 * unit, 1 * unit);
@@ -166,6 +205,26 @@ function drawBody() {
     ctx.fillRect(9 * unit, 41 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(11 * unit, 41 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(13 * unit, 41 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(4 * unit, 53 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(3 * unit, 54 * unit, 3 * unit, 1 * unit);
+    ctx.fillRect(7 * unit, 54 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(2 * unit, 55 * unit, 6 * unit, 1 * unit);
+    ctx.fillRect(10 * unit, 55 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(2 * unit, 56 * unit, 9 * unit, 1 * unit);
+    ctx.fillRect(13 * unit, 56 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(2 * unit, 57 * unit, 12 * unit, 1 * unit);
+    ctx.fillRect(2 * unit, 58 * unit, 6 * unit, 1 * unit);
+    ctx.fillRect(9 * unit, 58 * unit, 6 * unit, 1 * unit);
+    ctx.fillRect(4 * unit, 59 * unit, 2 * unit, 1 * unit);
+    ctx.fillRect(7 * unit, 59 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(9 * unit, 59 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(11 * unit, 59 * unit, 5 * unit, 1 * unit);
+    ctx.fillRect(4 * unit, 60 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(11 * unit, 60 * unit, 3 * unit, 1 * unit);
+    ctx.fillRect(15 * unit, 60 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(11 * unit, 61 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(13 * unit, 61 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(15 * unit, 61 * unit, 1 * unit, 1 * unit);
     ctx.fillStyle = "rgb(51,136,194)";// light sky blue
     ctx.fillRect(15 * unit, 36 * unit, 1 * unit, 4 * unit);
     ctx.fillRect(16 * unit, 38 * unit, 1 * unit, 2 * unit);
@@ -178,6 +237,19 @@ function drawBody() {
     ctx.fillRect(34 * unit, 37 * unit, 5 * unit, 1 * unit);
     ctx.fillRect(38 * unit, 38 * unit, 2 * unit, 1 * unit);
     ctx.fillRect(39 * unit, 39 * unit, 2 * unit, 1 * unit);
+    ctx.fillRect(15 * unit, 55 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(16 * unit, 57 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(17 * unit, 55 * unit, 1 * unit, 7 * unit);
+    ctx.fillRect(18 * unit, 57 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(19 * unit, 57 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(20 * unit, 56 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(21 * unit, 57 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(22 * unit, 56 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(23 * unit, 55 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(24 * unit, 56 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(25 * unit, 55 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(39 * unit, 57 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(40 * unit, 56 * unit, 1 * unit, 2 * unit);
     ctx.fillStyle = "rgb(145,145,145)";// grey
     ctx.fillRect(22 * unit, 35 * unit, 3 * unit, 1 * unit);
     ctx.fillRect(24 * unit, 36 * unit, 2 * unit, 1 * unit);
@@ -214,6 +286,20 @@ function drawBody() {
     ctx.fillRect(11 * unit, 47 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(12 * unit, 47 * unit, 1 * unit, 3 * unit);
     ctx.fillRect(13 * unit, 47 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(2 * unit, 59 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(3 * unit, 59 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(4 * unit, 61 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(5 * unit, 60 * unit, 1 * unit, 6 * unit);
+    ctx.fillRect(6 * unit, 59 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(7 * unit, 60 * unit, 1 * unit, 6 * unit);
+    ctx.fillRect(8 * unit, 58 * unit, 1 * unit, 7 * unit);
+    ctx.fillRect(9 * unit, 60 * unit, 1 * unit, 6 * unit);
+    ctx.fillRect(10 * unit, 59 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(11 * unit, 62 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(12 * unit, 61 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(13 * unit, 62 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(14 * unit, 60 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(15 * unit, 62 * unit, 1 * unit, 3 * unit);
     ctx.fillStyle = "rgb(10,50,180)";// light blue
     ctx.fillRect(15 * unit, 40 * unit, 2 * unit, 1 * unit);
     ctx.fillRect(19 * unit, 40 * unit, 1 * unit, 1 * unit);
@@ -228,7 +314,7 @@ function drawBody() {
     ctx.fillRect(15 * unit, 46 * unit, 5 * unit, 1 * unit);
     ctx.fillRect(21 * unit, 46 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(23 * unit, 46 * unit, 3 * unit, 1 * unit);
-    ctx.fillRect(15 * unit, 47 * unit, 2 * unit, 1 * unit);
+    ctx.fillRect(15 * unit, 47 * unit, 2 * unit, 2 * unit);
     ctx.fillRect(18 * unit, 47 * unit, 2 * unit, 1 * unit);
     ctx.fillRect(21 * unit, 47 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(23 * unit, 47 * unit, 1 * unit, 1 * unit);
@@ -237,6 +323,26 @@ function drawBody() {
     ctx.fillRect(39 * unit, 43 * unit, 1 * unit, 1 * unit);
     ctx.fillRect(39 * unit, 44 * unit, 2 * unit, 2 * unit);
     ctx.fillRect(40 * unit, 46 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(16 * unit, 60 * unit, 1 * unit, 6 * unit);
+    ctx.fillRect(17 * unit, 62 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(18 * unit, 62 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(19 * unit, 61 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(20 * unit, 60 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(21 * unit, 61 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(22 * unit, 59 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(23 * unit, 60 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(24 * unit, 59 * unit, 1 * unit, 9 * unit);
+    ctx.fillRect(25 * unit, 58 * unit, 1 * unit, 8 * unit);
+    ctx.fillRect(26 * unit, 64 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(27 * unit, 64 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(28 * unit, 64 * unit, 1 * unit, 4 * unit);
+    ctx.fillRect(29 * unit, 65 * unit, 1 * unit, 3 * unit);
+    ctx.fillRect(30 * unit, 65 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(31 * unit, 65 * unit, 1 * unit, 2 * unit);
+    ctx.fillRect(37 * unit, 64 * unit, 3 * unit, 1 * unit);
+    ctx.fillRect(38 * unit, 65 * unit, 1 * unit, 1 * unit);
+    ctx.fillRect(39 * unit, 59 * unit, 1 * unit, 5 * unit);
+    ctx.fillRect(40 * unit, 58 * unit, 1 * unit, 5 * unit);
     ctx.fillStyle = "rgb(144,105,97)";// light brown
     ctx.fillRect(34 * unit, 38 * unit, 4 * unit, 1 * unit);
     ctx.fillRect(30 * unit, 39 * unit, 8 * unit, 1 * unit);
@@ -247,6 +353,7 @@ function drawBody() {
     ctx.fillRect(24 * unit, 43 * unit, 9 * unit, 1 * unit);
     ctx.fillRect(25 * unit, 44 * unit, 5 * unit, 1 * unit);
     ctx.fillRect(26 * unit, 45 * unit, 2 * unit, 19 * unit);
+    ctx.fillRect(39 * unit, 47 * unit, 1 * unit, 10 * unit);
     ctx.fillStyle = "rgb(246,236,224)";// lightest brown
     ctx.fillRect(33 * unit, 43 * unit, 5 * unit, 1 * unit);
     ctx.fillRect(30 * unit, 44 * unit, 8 * unit, 1 * unit);
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var canvas;
 var ctx;
 var unit = 10;
+var requestId;
+var positionX = 0;
 window.onload = init; // When the window loads, run the init function
 function init() {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
-    drawLlama();
-   
+      drawLlama(-1,0);
+    //startAnimation();
+}
 
-                    
+
+function startAnimation() {
+    requestId = requestAnimationFrame(animationLoop);
+}
+
+function animationLoop(timeStamp) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawLlama (positionX,0);
+    positionX += 1;
+    requestId = requestAnimationFrame(animationLoop);
+    
 }
